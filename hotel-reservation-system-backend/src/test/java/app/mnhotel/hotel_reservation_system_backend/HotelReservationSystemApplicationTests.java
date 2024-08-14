a5d5c6c2ad13e6f2e489ed72cebc3c1ef892413e
lima73777@gmail.com <lima73777@gmail.com>
2024-08-14 14:18:10 -0300

    primer commit creacion de project backend springboot+maven+jpa+postgresql

---
+++ hotel-reservation-system-backend/src/test/java/app/mnhotel/hotel_reservation_system_backend/HotelReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mnhotel.hotel_reservation_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
