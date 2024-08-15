5fabbad93af007677d1779b2accec94fbc00259a
DESKTOP-9UJEHGC\maxi <nahuel_maxi11@hotmail.com>
2024-08-15 17:50:13 -0300

    color header

--- hotel-reservation-system-frontend/src/components/Header.tsx
+++ hotel-reservation-system-frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'; // Importa Link
 
 export default function Header() {
   return (
-    <header className="bg-white">
+    <header className="bg-orange-200">
   <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
     <a className="block text-orange-600" href="#">
       <span className="sr-only">Home</span>
