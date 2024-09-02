c7b6a4492fe96755303ab70ba4c54ead46aff813
lima73777@gmail.com <lima73777@gmail.com>
2024-09-02 20:43:58 -0300

    service imp de reservation

--- hotel-reservation-system-backend/src/main/java/app/mnhotel/hotel_reservation_system_backend/service/ReservationService.java
+++ hotel-reservation-system-backend/src/main/java/app/mnhotel/hotel_reservation_system_backend/service/ReservationService.java
@@ -1,5 +1,14 @@
 package app.mnhotel.hotel_reservation_system_backend.service;
 
-public interface ReservationService {
+import java.util.List;
+import java.util.Optional;
 
+import app.mnhotel.hotel_reservation_system_backend.dto.ReservationDTO;
+
+public interface ReservationService {
+	ReservationDTO createReservation(ReservationDTO reservationDTO)throws Exception;
+    ReservationDTO updateReservation(Long id, ReservationDTO reservationDTO)throws Exception;
+    void deleteReservation(Long id)throws Exception;
+    Optional<ReservationDTO> getReservationById(Long id)throws Exception;
+    List<ReservationDTO> getAllReservations()throws Exception;
 }
